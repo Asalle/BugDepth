0dc46378674a63e7b10279c953399d78aa42d650
Asalle <asalle.kim@gmail.com>
2018-03-12 19:26:20 +0100

    Add cmake files stubs

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Remove the '-DBUILD_TESTING:BOOL=TRUE' flag if you don't want the test to be built
+rm -rf build &> /dev/null
+mkdir build
+cd build
+cmake -DBUILD_TESTING:BOOL=TRUE ..
+make
