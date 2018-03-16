815d47932c95609f90e24cd3303ed01818a43dff
Asalle <asalle.kim@gmail.com>
2018-03-16 09:05:42 +0100

    Move setting Qt5_DIR to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export Qt5_DIR="/opt/qt/5.10.1/gcc_64/lib/cmake/Qt5/"
+
 # Remove the '-DBUILD_TESTING:BOOL=TRUE' flag if you don't want the test to be built
 #rm -rf build &> /dev/null
 #mkdir build
