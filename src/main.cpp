7140bc214bbc867dd745390251c58a6df6bda66e
Asalle <asalle.kim@gmail.com>
2018-03-27 19:25:02 +0200

    Refactor main

--- src/main.cpp
+++ src/main.cpp
@@ -18,32 +18,34 @@ auto main() -> int
 {
     auto filenames = bugDepth::ArgParser::prepareImageFileNames("res/bug/");
 
-    QImage sampleImage(filenames.front().c_str());
-    const uint width = sampleImageMat.width();
-    const uint height = sampleImageMat.height();
+    QImage sampleQImage(filenames.front().c_str());
+    const uint width = sampleQImage.width();
+    const uint height = sampleQImage.height();
 
     bugDepth::EdgeDetector detector;
     bugDepth::Accumulator accumulator(width, height);
+
     int depth = 0x10;
     for (auto& filename: filenames)
     {
-        QImage readImg(filename.c_str());
-        bugDepth::RgbImg testImage(width, height, readImg.bits());
+        QImage inImage(filename.c_str());
+        bugDepth::RgbImg testImage(width, height, inImage.bits());
         bugDepth::GrayImg edges = detector.sobel(testImage);
         accumulator.accumulate(testImage, edges, depth);
-        depth += 0x10;
+        depth += 0x20;
     }
 
-    bugDepth::RgbImg sampleImage(width, height, sampleImageMat.bits());
+    bugDepth::RgbImg sampleImage(width, height, sampleQImage.bits());
     accumulator.setBg(sampleImage);
+
     auto resultdir = prepareResultDir();
 
-    auto res = accumulator.getSharpImage();
-    QImage result(res.getData(), res.getWidth(), res.getHeight(), QImage::Format_RGB32);
-    result.save(resultdir.filePath("result.png"));
+    auto sharpImage = accumulator.getSharpImage();
+    QImage sharpQImage(sharpImage.getData(), width, height, QImage::Format_RGB32);
+    sharpQImage.save(resultdir.filePath("result.png"));
 
-    auto resDepthMap = accumulator.getDepthMap();
-    QImage resultDepth(resDepthMap.getData(), resDepthMap.getWidth(), resDepthMap.getHeight(), QImage::Format_Grayscale8);
-    resultDepth.save(resultdir.filePath("depthMap.png"));
+    auto depthMap = accumulator.getDepthMap();
+    QImage depthMapQImage(depthMap.getData(), width, height, QImage::Format_Grayscale8);
+    depthMapQImage.save(resultdir.filePath("depthMap.png"));
     return 0;
 }
